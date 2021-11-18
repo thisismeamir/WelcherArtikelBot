388a2b84fddb7143f2b633a258199c7ac40e5362
Amir <thisismeamir@outlook.com>
2021-11-18 18:33:12 +0330

    bot connected and ready for code

--- src/bot.py
+++ src/bot.py
@@ -1,15 +1,24 @@
 from dotenv import load_dotenv
 import os
+import telebot
+
 
 #----- API KEY importing -----
 load_dotenv()
 __API_KEY = os.getenv('API_KEY')
+bot = telebot.TeleBot(__API_KEY)
 
 #----- BOT body -----
 def main():
-     pass
+     @bot.message_handler(commands=['start'])
+     def start(message):
+          # Here should be some initializing, database adding and timer starting codes
+          print("Someone Started")
+          bot.reply_to(message, "Welcome")
+     bot.infinity_polling()          
 
-
+if __name__ == '__main__':
+     main()
 
 
 
