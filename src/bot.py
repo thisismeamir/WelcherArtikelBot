a228b147ef936ea6ddbd28746298e6cdac1dffa2
Amir <thisismeamir@outlook.com>
2021-11-18 18:13:23 +0330

    gitignore env apikey bot.py updated

---
+++ src/bot.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+import os
+
+#----- API KEY importing -----
+load_dotenv()
+__API_KEY = os.getenv('API_KEY')
+
+#----- BOT body -----
+def main():
+     pass
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+     main()
